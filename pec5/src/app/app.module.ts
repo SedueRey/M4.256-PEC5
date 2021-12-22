a54f5686ebfe0689d4f7a3e6ac9bf2bc20a56a91
Eduardo - Sedue - Rey <proyectos@seduerey.com>
2021-12-22 18:13:14 +0100

    Service worker y material, a espera de diseño

--- pec5/src/app/app.module.ts
+++ pec5/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { ImagesComponent } from './components/images/images.component';
 import { ImageComponent } from './components/image/image.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,14 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
