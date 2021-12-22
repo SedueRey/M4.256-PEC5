28f28b3ceff535bcb082d3ecc0c62a249cbe7adc
Eduardo - Sedue - Rey <proyectos@seduerey.com>
2021-12-22 17:39:42 +0100

    Añadidos servicios y componentes

---
+++ pec5/src/app/components/image/image.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ImagesService } from 'src/app/services/images.service';
+import { Image } from 'src/app/models/image.interface';
+
+@Component({
+  selector: 'app-image',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.css']
+})
+export class ImageComponent implements OnInit {
+
+  image: Image;
+
+  constructor(
+    private imagesService: ImagesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    const identifier = this.activatedRoute.snapshot.paramMap.get('id')
+
+    this.imagesService.getImageById(identifier).subscribe((image) => {
+      if (!image) {
+        return this.router.navigateByUrl('/');
+      }
+
+      this.image = image;
+    });
+  }
+
+}
